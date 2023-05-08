51104ce96daffbf50d7ad7c3dc7726065314f403
Mac <vscherer86@gmail.com>
2023-05-08 20:21:48 -0300

    Note Adapter

---
+++ app/src/main/java/com/example/mvvmsampleapp/AddNoteActivity.java
@@ -0,0 +1,84 @@
+package com.example.mvvmsampleapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddNoteActivity extends AppCompatActivity {
+    public static final String EXTRA_TITLE =
+            "com.example.mvvmsampleapp.EXTRA_TITLE";
+    public static final String EXTRA_DESCRIPTION =
+            "com.example.mvvmsampleapp.EXTRA_TITLE";
+    public static final String EXTRA_PRIORITY =
+            "com.example.mvvmsampleapp.EXTRA_TITLE";
+    private EditText editTextTitle;
+    private EditText editTextDescription;
+    private NumberPicker numberPickerPriority;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextDescription = findViewById(R.id.edit_text_Description);
+        numberPickerPriority = findViewById(R.id.number_picker_priority);
+
+        numberPickerPriority.setMinValue(1);
+        numberPickerPriority.setMinValue(10);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add note");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //switch (item.getItemId()) {
+            //case R.id.save_note:
+        if (item.getItemId()==R.id.save_note){
+            saveNote();
+            return true;
+        }else{
+            return super.onOptionsItemSelected(item);
+        }
+
+
+            //default:
+
+        //}
+    }
+
+    private void saveNote() {
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
+        int priority = numberPickerPriority.getValue();
+
+        if (title.trim().isEmpty() || description.trim().isEmpty()){
+            Toast.makeText(this, "please insert a title and description", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent data = new Intent();
+        data.putExtra(EXTRA_TITLE, title);
+        data.putExtra(EXTRA_DESCRIPTION, description);
+        data.putExtra(EXTRA_PRIORITY, priority);
+
+        setResult(RESULT_OK, data);
+        finish();
+    }
+}
